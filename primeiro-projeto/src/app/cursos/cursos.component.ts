8d8335b93105daa94b9ade1003ad641e5b6a7649
Renan Maciel Lima <rmaciell@everis.com>
2018-06-14 17:30:46 -0300

    Components e Services

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[] = ["Java", "Javascript", "Typescript", "Angular5"];
+
+  constructor() {
+    this.nomePortal = "Renan Site";
+  }
 
   ngOnInit() {
   }
