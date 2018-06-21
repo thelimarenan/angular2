ee4c7f977f98ccb1e1a11c2bba72442f12d36ca0
Renan Maciel Lima <rmaciell@everis.com>
2018-06-21 19:12:58 -0300

    Data-bind + event-bind + two-way data-binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styles: [ 
+    `
+      .highlight {
+        background-color: yellow;
+        font-weight: bold;
+      }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
@@ -11,12 +18,40 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem: string = "https://www.everis.com/sites/all/themes/everis/logo.png";
 
+  valorAtual: string = "";
+  valorSalvo;
+
+  isMouseOver: boolean = false;
+
+  nome: string = "abc";
+
+  pessoa: any = {
+    nome: "Renan",
+    idade: 27
+  }
+
   getValor() {
     return 1;
   }
 
   getCurtirCusro() {
     return true;
+  }
+
+  botaoClicado() {
+    alert("Teste do event binding");
+  }
+
+  salvarValor(value){
+    this.valorSalvo = value;
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
   
   constructor() { }
